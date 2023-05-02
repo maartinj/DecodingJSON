14203c3a578a123f561ebbb7f26fc2d0b6adef04
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-03 01:24:48 +0200

    Completed JSON Decoding Playground for Swift

--- Decoding Tester.playground/Pages/DecodeFromBundle.xcplaygroundpage/Contents.swift
+++ Decoding Tester.playground/Pages/DecodeFromBundle.xcplaygroundpage/Contents.swift
@@ -10,14 +10,42 @@ import Foundation
 
  #### Start by entering your model below
 */
+struct User: Codable {
+  struct Address: Codable {
+    struct Geo: Codable {
+      let lat: String
+      let lng: String
+    }
 
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: Geo
+  }
+
+  struct Company: Codable {
+    let name: String
+    let catchPhrase: String
+    let bs: String
+  }
+
+  let id: Int
+  let name: String
+  let username: String
+  let email: String
+  let address: Address
+  let phone: String
+  let website: String
+  let company: Company
+}
 /*:
  ### Decode your JSON on the next line making sure you use the correct model or array of model objects to decode as well as the correct name for the json file
  ```
  var users = Bundle.main.decode([User].self, from: "Users.json")
  ```
  */
-
+let users = Bundle.main.decode([User].self, from: "Users.json")
 /*:
 ### Test in the lines following
  ```
@@ -29,3 +57,9 @@ import Foundation
  }
  ```
  */
+print(users.count)
+for user in users {
+    print(user.name)
+    print(user.address.city)
+    print("----------------")
+}
--- Decoding Tester.playground/Pages/DecodeFromDocDir.xcplaygroundpage/Contents.swift
+++ Decoding Tester.playground/Pages/DecodeFromDocDir.xcplaygroundpage/Contents.swift
@@ -17,14 +17,42 @@ print(FileManager.docDirPath)
 /*:
  #### Start by entering your model below
 */
+struct User: Codable {
+  struct Address: Codable {
+    struct Geo: Codable {
+      let lat: String
+      let lng: String
+    }
 
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: Geo
+  }
+
+  struct Company: Codable {
+    let name: String
+    let catchPhrase: String
+    let bs: String
+  }
+
+  let id: Int
+  let name: String
+  let username: String
+  let email: String
+  let address: Address
+  let phone: String
+  let website: String
+  let company: Company
+}
 /*:
  ### Decode your JSON on the next line making sure you use the correct model or array of model objects to decode as well as the correct name for the json file.
  ```
  var users = FileManager.decode([User].self, from: "Users.json")
  ```
  */
-
+var users = FileManager.decode([User].self, from: "Users.json")
 /*:
 ### Test in the lines following
  ```
@@ -36,4 +64,9 @@ print(FileManager.docDirPath)
  }
  ```
  */
-
+print(users.count)
+for user in users {
+    print(user.name)
+    print(user.address.city)
+    print("----------------")
+}
